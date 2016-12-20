ed6b539250824640bb64e5f3ff4b4700f4c954af
ShravyaVanchanagiri <shravya.vanchanagiri@india.semanticbits.com>
2016-12-20 14:11:24 +0530

    pushing index file

---
+++ script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vanchanagiri shravya on 12/19/2016.
+ */
+function validate() {
+    var val=document.form1.i1;
+    if(val.value == "shravya"){
+        alert("Welcome");
+    }
+    else{
+        alert("Invalid Name");
+    }
+}
+function welcome() {
+    alert("Hey... It's my pic");
+}
+function bye() {
+    alert("Bye Bye....");
+}
